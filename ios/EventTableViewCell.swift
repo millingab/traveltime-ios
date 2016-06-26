af9057342656d883ca322fc7656ee697285f8b57
Millin Gabani <gabanimillin@gmail.com>
2016-06-25 21:43:58 -0400

    Created eventTableViewCell

---
+++ ios/EventTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventTableViewCell.swift
+//  ios
+//
+//  Created by Millin Gabani on 2016-06-25.
+//  Copyright © 2016 Millin Gabani. All rights reserved.
+//
+
+import Cocoa
+
+class EventTableViewCell: UITableViewCell {
+
+}
